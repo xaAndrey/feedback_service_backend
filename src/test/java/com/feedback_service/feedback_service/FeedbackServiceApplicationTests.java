bed51e2b3a1a47cfb1bb47889fd54d930914ffc0
andreypisarev <xaandreynx@gmail.com>
2023-07-04 09:49:54 +0300

    initial project

---
+++ src/test/java/com/feedback_service/feedback_service/FeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback_service.feedback_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
