f851ffd7cded856f742fbba19b44c52529aca0e7
andreypisarev <xaandreynx@gmail.com>
2023-07-04 18:37:28 +0300

    [feat]<repositories>: create the repository for Registration

---
+++ src/main/java/com/feedback_service/feedback_service/RegistrationRepository.java
@@ -0,0 +1,7 @@
+package com.feedback_service.feedback_service;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegistrationRepository extends CrudRepository<Registration, Integer> {
+
+}
