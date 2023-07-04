3ef14c526295eb4ae0368681ca119e5fd7146c79
andreypisarev <xaandreynx@gmail.com>
2023-07-04 13:24:18 +0300

    [feat]<repository>:Create te repository for user

---
+++ src/main/java/com/feedback_service/feedback_service/UserRepository.java
@@ -0,0 +1,7 @@
+package com.feedback_service.feedback_service;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
