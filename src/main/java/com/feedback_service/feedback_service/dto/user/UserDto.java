d119e157a901cdfdbf2a9bcba3c1840ec739f173
andreypisarev <xaandreynx@gmail.com>
2023-07-19 12:23:52 +0300

    [feat](dto): add dto for User

---
+++ src/main/java/com/feedback_service/feedback_service/dto/user/UserDto.java
@@ -0,0 +1,29 @@
+package com.feedback_service.feedback_service.dto.user;
+
+import com.feedback_service.feedback_service.entity.registration.RegistrationEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDto {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String securityStamp;
+
+    private ZonedDateTime registrationDate;
+
+    private boolean accountLocked;
+
+    private List<RegistrationEntity> registrationEntities;
+}
