25dc43136212ed4834e14b526321f893d63add83
andreypisarev <xaandreynx@gmail.com>
2023-07-20 11:44:48 +0300

    [feat](controller): create controller for User

---
+++ src/main/java/com/feedback_service/feedback_service/controller/v1/UserController.java
@@ -0,0 +1,54 @@
+package com.feedback_service.feedback_service.controller.v1;
+
+import com.feedback_service.feedback_service.dto.user.CreateUserDto;
+import com.feedback_service.feedback_service.dto.user.UserDto;
+import com.feedback_service.feedback_service.entity.user.UserEntity;
+import com.feedback_service.feedback_service.exception.UserAlreadyExistException;
+import com.feedback_service.feedback_service.exception.UserNotFoundException;
+import com.feedback_service.feedback_service.repository.user.UserRepository;
+import com.feedback_service.feedback_service.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/api/v1/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUser() {
+        return ResponseEntity.ok(userService.findUserDtoAll());
+    }
+
+    @GetMapping(path = "/user/{id}")
+    public ResponseEntity<UserDto> getById(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(userService.findUserDtoById(id));
+        } catch (UserNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PostMapping(path = "/user")
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserDto newUser) {
+        try {
+            return ResponseEntity.ok(userService.createUserDto(newUser));
+        } catch (UserAlreadyExistException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+}
