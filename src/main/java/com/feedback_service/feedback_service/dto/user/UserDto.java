5130220acd643dcf547a20ff1b1d79cbdb6c0010
andreypisarev <xaandreynx@gmail.com>
2023-07-20 11:35:21 +0300

    [style](dto): correct code style

--- src/main/java/com/feedback_service/feedback_service/dto/user/UserDto.java
+++ src/main/java/com/feedback_service/feedback_service/dto/user/UserDto.java
@@ -12,18 +12,11 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class UserDto {
-
     private Integer id;
-
     private String username;
-
     private String password;
-
     private String securityStamp;
-
     private ZonedDateTime registrationDate;
-
     private boolean accountLocked;
-
     private List<RegistrationEntity> registrationEntities;
 }
