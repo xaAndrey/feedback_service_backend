85aec12d826c57e3ead26f0b6e24f094b25cb3db
andreypisarev <xaandreynx@gmail.com>
2023-07-25 00:23:29 +0300

    [feat](exception): create RegistrationNotFound exception

---
+++ src/main/java/com/feedback_service/feedback_service/exception/RegistrationNotFound.java
@@ -0,0 +1,7 @@
+package com.feedback_service.feedback_service.exception;
+
+public class RegistrationNotFound extends RuntimeException {
+    public RegistrationNotFound(String msg) {
+        super(msg);
+    }
+}
