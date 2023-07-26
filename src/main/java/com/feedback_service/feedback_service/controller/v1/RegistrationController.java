5751159ee22c1e8c268cf3c182d866ff25b3e993
andreypisarev <xaandreynx@gmail.com>
2023-07-26 16:02:42 +0300

    [feat](controller): create RegistrationController

---
+++ src/main/java/com/feedback_service/feedback_service/controller/v1/RegistrationController.java
@@ -0,0 +1,58 @@
+package com.feedback_service.feedback_service.controller.v1;
+
+import com.feedback_service.feedback_service.dto.registration.CreateRegistrationDto;
+import com.feedback_service.feedback_service.dto.registration.RegistrationDto;
+import com.feedback_service.feedback_service.dto.registration.UpdateRegistrationDto;
+import com.feedback_service.feedback_service.entity.registration.RegistrationEntity;
+import com.feedback_service.feedback_service.service.registration.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/registrations")
+public class RegistrationController {
+    private final RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @GetMapping
+    public List<RegistrationDto> findAllByOrderById(@RequestParam Integer page, @RequestParam Integer size) {
+        return registrationService.findAllRegistrationDtoByOrderById(page, size);
+    }
+
+    @GetMapping(path = "/registration/{id}")
+    public ResponseEntity<RegistrationDto> getAllRegistration(@PathVariable Integer id) {
+        return ResponseEntity.ok(registrationService.findRegistrationDtoById(id));
+    }
+
+    @PutMapping(path="/registration/{id}")
+    public ResponseEntity<RegistrationEntity> updateRegistrationById(@PathVariable Integer id,
+                                                                     @RequestBody UpdateRegistrationDto updateRegistrationDto){
+        RegistrationEntity updateRegistrationEntity = registrationService.findRegistrationEntityById(id);
+
+        updateRegistrationEntity.setRegistered(updateRegistrationDto.isRegistered());
+        return ResponseEntity.ok(updateRegistrationEntity);
+
+    }
+
+    @PostMapping(path = "/registration")
+    public ResponseEntity<RegistrationDto> createRegistration(
+            @RequestBody CreateRegistrationDto newRegistration
+    ) throws ResponseStatusException
+    {
+        try {
+            return ResponseEntity.ok(registrationService.createRegistrationDto(newRegistration, newRegistration.getUserId()));
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+}
