b70412eda9ed38bf50c8a6abd6ff41be7682b7e6
andreypisarev <xaandreynx@gmail.com>
2023-07-26 15:55:00 +0300

    [feat](dto): create UpdateRegistrationDto

---
+++ src/main/java/com/feedback_service/feedback_service/dto/registration/UpdateRegistrationDto.java
@@ -0,0 +1,12 @@
+package com.feedback_service.feedback_service.dto.registration;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UpdateRegistrationDto {
+    private boolean isRegistered;
+}
