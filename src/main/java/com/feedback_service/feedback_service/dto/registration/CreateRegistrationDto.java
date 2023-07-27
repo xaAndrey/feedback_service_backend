2959e2b48d866132d0f7d891f1f057184b51fa0a
andreypisarev <xaandreynx@gmail.com>
2023-07-28 01:23:17 +0300

    [refactor](dto): add annotations NotNull and NotBlank for fields

--- src/main/java/com/feedback_service/feedback_service/dto/registration/CreateRegistrationDto.java
+++ src/main/java/com/feedback_service/feedback_service/dto/registration/CreateRegistrationDto.java
@@ -1,5 +1,7 @@
 package com.feedback_service.feedback_service.dto.registration;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +10,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class CreateRegistrationDto {
+    @NotBlank
     private String fio;
+    @NotBlank
     private String phone;
+    @NotBlank
     private String doctor;
     private String comments;
+    @NotNull
     private Integer userId;
 }
