7f185cb4a1dce8d8f3c4c7673abf43a061a6de68
andreypisarev <xaandreynx@gmail.com>
2023-07-28 02:50:49 +0300

    [feat](jwt): add TokenPair for jwt

---
+++ src/main/java/com/feedback_service/feedback_service/configuration/jwt/tocken/TokenPair.java
@@ -0,0 +1,8 @@
+package com.feedback_service.feedback_service.configuration.jwt.tocken;
+
+public class TokenPair {
+    public String accessToken;
+    public Long accessTokenExpirationTime;
+    public String refreshToken;
+    public Long refreshTokenExpirationTime;
+}
