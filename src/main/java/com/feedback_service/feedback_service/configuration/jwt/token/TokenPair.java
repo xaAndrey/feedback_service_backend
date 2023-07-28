170ef7ed8084d0a51beb6632849120b273f50442
andreypisarev <xaandreynx@gmail.com>
2023-07-28 03:31:03 +0300

    [feat](jwt): add setters, getters and constructor for TokenPair class

--- src/main/java/com/feedback_service/feedback_service/configuration/jwt/token/TokenPair.java
+++ src/main/java/com/feedback_service/feedback_service/configuration/jwt/token/TokenPair.java
@@ -5,4 +5,47 @@ public class TokenPair {
     public Long accessTokenExpirationTime;
     public String refreshToken;
     public Long refreshTokenExpirationTime;
+
+    public TokenPair(String accessToken,
+                     Long accessTokenExpirationTime,
+                     String refreshToken,
+                     Long refreshTokenExpirationTime
+    ) {
+        this.accessToken = accessToken;
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+        this.refreshToken = refreshToken;
+        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Long getAccessTokenExpirationTime() {
+        return accessTokenExpirationTime;
+    }
+
+    public void setAccessTokenExpirationTime(Long accessTokenExpirationTime) {
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Long getRefreshTokenExpirationTime() {
+        return refreshTokenExpirationTime;
+    }
+
+    public void setRefreshTokenExpirationTime(Long refreshTokenExpirationTime) {
+        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
+    }
 }
