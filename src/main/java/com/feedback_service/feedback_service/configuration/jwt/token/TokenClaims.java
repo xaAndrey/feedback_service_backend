21ef0760a23f5c90531d06ab9dd22172541b55d8
andreypisarev <xaandreynx@gmail.com>
2023-07-28 03:42:32 +0300

    [feat](jwt): add getter, setters and constructor for TokenClaims class

--- src/main/java/com/feedback_service/feedback_service/configuration/jwt/token/TokenClaims.java
+++ src/main/java/com/feedback_service/feedback_service/configuration/jwt/token/TokenClaims.java
@@ -4,4 +4,36 @@ public class TokenClaims {
     public Long id;
     public String securityStamp;
     public TokenType tokenType;
+
+    public TokenClaims(Long id,
+                       String securityStamp,
+                       TokenType tokenType) {
+        this.id = id;
+        this.securityStamp = securityStamp;
+        this.tokenType = tokenType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSecurityStamp() {
+        return securityStamp;
+    }
+
+    public void setSecurityStamp(String securityStamp) {
+        this.securityStamp = securityStamp;
+    }
+
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(TokenType tokenType) {
+        this.tokenType = tokenType;
+    }
 }
