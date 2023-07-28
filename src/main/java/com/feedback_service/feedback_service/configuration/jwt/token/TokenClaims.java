cab71ccb58e152c33e97fc4b69d94c476608cc1c
andreypisarev <xaandreynx@gmail.com>
2023-07-28 03:35:59 +0300

    [feat](jwt): create TokenClaims

---
+++ src/main/java/com/feedback_service/feedback_service/configuration/jwt/token/TokenClaims.java
@@ -0,0 +1,7 @@
+package com.feedback_service.feedback_service.configuration.jwt.token;
+
+public class TokenClaims {
+    public Long id;
+    public String securityStamp;
+    public TokenType tokenType;
+}
