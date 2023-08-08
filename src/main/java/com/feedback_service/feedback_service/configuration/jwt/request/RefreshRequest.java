0e5e68eeae844aca0041dd727c3fdc33e7e31157
andreypisarev <xaandreynx@gmail.com>
2023-08-08 10:55:28 +0300

    [feat]<jwt>: add dto RefreshRequest

---
+++ src/main/java/com/feedback_service/feedback_service/configuration/jwt/request/RefreshRequest.java
@@ -0,0 +1,9 @@
+package com.feedback_service.feedback_service.configuration.jwt.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public class RefreshRequest {
+    @NotEmpty
+    public String refreshToken;
+}
