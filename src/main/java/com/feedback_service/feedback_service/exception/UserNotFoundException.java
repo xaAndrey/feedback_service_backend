272a6621747c64f386476bfb3e401a2cf7031948
andreypisarev <xaandreynx@gmail.com>
2023-08-09 14:27:56 +0300

    [feat]<error>: change the UNF exception

--- src/main/java/com/feedback_service/feedback_service/exception/UserNotFoundException.java
+++ src/main/java/com/feedback_service/feedback_service/exception/UserNotFoundException.java
@@ -1,9 +1,29 @@
 package com.feedback_service.feedback_service.exception;
 
-public class UserNotFoundException extends RuntimeException {
+import com.feedback_service.feedback_service.util.error.ErrorCode;
+import org.springframework.http.HttpStatus;
 
-    public UserNotFoundException(String msg) {
-        super(msg);
+public class UserNotFoundException extends ResponseError {
+
+    public UserNotFoundException(
+            String message,
+            HttpStatus status,
+            String errorCode) {
+        super(message, status, errorCode);
+    }
+
+    public static void registrationNotFoundError(
+            String message,
+            HttpStatus status,
+            String errorCode
+    ) {
+        if (message != null &&
+                status != null &&
+                errorCode != null) {
+            throw new RegistrationNotFoundException(message, status, errorCode);
+        } else if (message != null) {
+            throw new RegistrationNotFoundException(message, HttpStatus.NOT_FOUND, ErrorCode.UNF);
+        }
     }
 }
 
